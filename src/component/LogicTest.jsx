eb1d058b853fd13547729f797d4fda2d897610ab
basudevkumar <jhulonkumar40@gmail.com>
2026-03-25 17:24:10 +0600

    text filter by discoutnt precent

--- src/component/LogicTest.jsx
+++ src/component/LogicTest.jsx
@@ -1,60 +1,72 @@
 import React, { useMemo, useState } from "react";
 import useAllProduct from "./useAllProduct";
 import Cart from "./cart";
+import { number } from "zod";
 
 const LogicTest = () => {
-  const [hasBrand, setHasBrand] = useState([]);
+  const [discount, setDiscount] = useState([]);
+
+  const discountRanges = [
+    { label: "0%-10%", value: "0-10" },
+    { label: "11%-20%", value: "11-20" },
+    { label: "21%-30%", value: "21-30" },
+    { label: "31%-40%", value: "31-40" },
+    { label: "41%-50%", value: "41-50" },
+    { label: "51% or more", value: "51-100" },
+  ];
 
   const { data, isLoading, isError } = useAllProduct();
 
-  const filterByBrand = useMemo(() => {
-    if (!data) return [];
-
-    if (hasBrand.length === 0) {
-      return data;
-    }
-
-    const filteredData = data?.filter((items) => {
-      return hasBrand.includes(items?.brand);
-    });
-
-    return filteredData;
-  }, [hasBrand, data]);
-
-  const brand = [
-    ...new Set(data?.map((items) => items?.brand)?.filter(Boolean)),
-  ];
-
-  const handleChange = (e) => {
-    const { value, checked } = e.target;
+  const handleChages = (e) => {
+    let { checked, value } = e.target;
 
     if (checked) {
-      setHasBrand((prev) => [...prev, value]);
+      setDiscount((prev) => [...prev, value]);
     } else {
-      setHasBrand((prev) => prev?.filter((items) => items !== value));
+      setDiscount((prev) => prev.filter((items) => value !== items));
     }
   };
+
+  const filterRange = useMemo(() => {
+    if (!data) return [];
+    if (discount.length === 0) return data;
+
+    // const [min, max] = discount.split("-").map((items) => items);
+    const ranger = discount.map((items) => {
+      const [min, max] = items.split("-").map(Number);
+      return { min, max };
+    });
+
+    const discountFilter = data?.filter((items) => {
+      return ranger.some((r) => {
+        return (
+          items.discountPercentage > r.min && items.discountPercentage < r.max
+        );
+      });
+    });
+
+    return discountFilter;
+    
+  }, [discount, data]);
 
   return (
     <div className="max-w-[1320px] mx-auto my-15">
       <div className="grid grid-cols-4 gap-x-5">
-        <div className="flex  items-start  flex-col gap-3">
-          {brand?.map((items, index) => {
-            return (
-              <label key={index} className="flex gap-2 cursor-pointer">
-                <input
-                  type="checkbox"
-                  value={items}
-                  onChange={handleChange}
-                  checked={hasBrand.includes(items)}
-                />
-                {items}
-              </label>
-            );
-          })}
+        <div className="flex items-start flex-col gap-3">
+          {discountRanges.map((range) => (
+            <label key={range.value} className="flex gap-x-3 cursor-pointer">
+              <input
+                type="checkbox"
+                value={range.value}
+                onChange={handleChages}
+                checked={discount.includes(range.value)}
+              />
+              {range.label}
+            </label>
+          ))}
         </div>
         <div className="grid grid-cols-3 gap-8 col-span-3">
-          {filterByBrand?.map((items, index) => {
+          {filterRange?.map((items, index) => {
             return (
               <Cart key={index} img={items?.thumbnail} title={items?.title} />
             );
