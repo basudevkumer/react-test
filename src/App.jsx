457a6d163d3e6853e15392824230ab0bee5cc8ff
basudevkumar <jhulonkumar40@gmail.com>
2026-04-07 08:49:27 +0600

    feat: handle derived full name without extra state

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [status, setStatus] = useState("idle");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  // const [fullName, setFullName] = useState(""); its a wrong approach
+  const fullName = firstName + " " + lastName; // good approach
   return (
-    <div>
-      <p className="text-lg px-5">{status}</p>
-      <button onClick={() => setStatus("Success")} className="px-3 py-1.5 border ">success</button>
-      <button onClick={() => setStatus("Start")} className="px-3 py-1.5 border ml-3">Start</button>
+    <div className="p-10">
+      <input
+        type="text"
+        className="border"
+        placeholder="first name"
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <input
+        type="text"
+        className="border"
+        placeholder="Last name"
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <p className="text-lg px-5">{fullName}</p>
     </div>
   );
 };
